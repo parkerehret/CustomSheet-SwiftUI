0f20f0b4c9182fd6363709b35be9670cefa864b5
Parker Ehret <113559287+beaconparker@users.noreply.github.com>
2023-07-10 08:36:20 -0700

    Initial Commit

---
+++ Custom Sheet/Custom_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_SheetApp.swift
+//  Custom Sheet
+//
+//  Created by Parker Ehret on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
